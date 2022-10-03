04a57dfa9b49469b51047dcc43d83ec1025904d4
RolandoRojas28 <rojasrolando027@gmail.com>
2022-10-03 17:42:38 -0500

    subiendo mi proyecto de spring boot

---
+++ src/main/java/com/rojas/RRojas/RRojasApplication.java
@@ -0,0 +1,13 @@
+package com.rojas.RRojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RRojasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RRojasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rojas/RRojas/RRojasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojas.RRojas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RRojasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
